565ff1b5e5eaa45e5f64b3f048d16565ae26fffc
federicasfc <fesafeco@fakeemail.com>
2022-03-03 12:01:16 -0500

    Added AcquiredList and ReadList DbSets to AppDbCtx; Created new migration/updated db with change; Discriminator column created and all subproperty columns added to ReadingList table in Azure, so inheritance looks successful so far

---
+++ BookTracker/Server/Data/Migrations/20220303165636_AddedListSubTypeDbSets.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookTracker.Server.Data.Migrations
+{
+    public partial class AddedListSubTypeDbSets : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "AcquiredUtc",
+                table: "ReadingLists",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "ReadingLists",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Format",
+                table: "ReadingLists",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "HowAcquired",
+                table: "ReadingLists",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFinished",
+                table: "ReadingLists",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Review",
+                table: "ReadingLists",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "StartedUtc",
+                table: "ReadingLists",
+                type: "datetimeoffset",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AcquiredUtc",
+                table: "ReadingLists");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "ReadingLists");
+
+            migrationBuilder.DropColumn(
+                name: "Format",
+                table: "ReadingLists");
+
+            migrationBuilder.DropColumn(
+                name: "HowAcquired",
+                table: "ReadingLists");
+
+            migrationBuilder.DropColumn(
+                name: "IsFinished",
+                table: "ReadingLists");
+
+            migrationBuilder.DropColumn(
+                name: "Review",
+                table: "ReadingLists");
+
+            migrationBuilder.DropColumn(
+                name: "StartedUtc",
+                table: "ReadingLists");
+        }
+    }
+}
